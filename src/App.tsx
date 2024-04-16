dfa6ffe2980db31fc485afa8e481f0bc6a37a16c
Darren Angle <darrendarrenangle@gmail.com>
2024-04-16 11:57:02 -0500

    latest

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,56 @@
-import { useState } from "react";
-
 import "./App.css";
 
 function App() {
   return (
     <>
-      <h3>DARREN ANGLE</h3>
+      <img className="ellipse" src="/ellipse.svg" alt="Ellipse Graphic" />
+      <h4>DARREN ANGLE</h4>
       <p>
-        I'm a software engineer and AI researcher with 12 years experience
-        building and managing teams in biotech, healthcare, and e-commerce. I
-        also have two poetry degrees. <br />
-        <a target="_blank" href="https://openreview.net/forum?id=0gl0SJtd2E">
+        Software engineer and AI researcher with 12+ years of experience
+        shipping code and managing teams in biotech, healthcare, and e-commerce.
+        Also: two kids and two poetry degrees.
+      </p>
+      <p>
+        <a target="_blank" href="./Darren.Angle.Resume.2024.pdf">
           Resume
         </a>
-        {" | "}
+        {" • "}
         <a target="_blank" href="https://github.com/darrenangle">
           Github
+        </a>
+        {" • "}
+        <a target="_blank" href="https://twitter.com/darrenangle">
+          Twitter
         </a>
       </p>
 
       <br />
-      <h3>FORTHCOMING</h3>
-      <p>Talk is Deep: Training LLMs on 1M Conversations With Themselves</p>
-      <br />
-      <h3>RECENT WORK</h3>
+      <h4>FORTHCOMING</h4>
       <p>
-        NeurIPS '23 Paper: Identifying Neglected Hypotheses In Neurodegenerative
-        Diseas With Large Language Models <br />
+        Talk is Deep: Training LLMs on 1M Academic Conversations With Themselves
+      </p>
+      <br />
+      <h4>RECENT WORK</h4>
+      <p>
+        Identifying Neglected Hypotheses In Neurodegenerative Disease With Large
+        Language Models {" •  "}
         <a target="_blank" href="https://openreview.net/forum?id=0gl0SJtd2E">
-          (Open Review)
+          NeurIPS '23 GenBio
         </a>
       </p>
       <p>
-        Lead AI Engineer: Ontology of Digital Health Technologies <br />
+        Ontology of Digital Health Technologies {" •  "}
         <a
           target="_blank"
           href="https://vaults.prism.bio/vaults/dht/Overview/Overview"
         >
-          (NeurIPS 23)
+          vaults.prism.bio
         </a>
       </p>
       <p>
-        Lead AI Engineer: Shopify Help Center AI Experience <br />
+        Shopify Help Center AI Experience {" •  "}
         <a target="_blank" href="https://help.shopify.com/en/">
-          (Shopify)
+          Shopify
         </a>
       </p>
     </>
